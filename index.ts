2b1ec4a65607262c51da00cc5c18e50a1d05197a
meadarsh <itsadarsh33@gmail.com>
2024-10-06 11:06:14 +0530

    fix

--- index.ts
+++ index.ts
@@ -600,6 +600,6 @@ const roomId =conversation.type === "COMMUNITY"
 
 // Start the server
 const PORT = process.env.PORT || 8000;
-server.listen(PORT,'0.0.0.0', () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
